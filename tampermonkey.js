c20509a0f5ca9d1e9dac9369f8a318f46145af7e
Davblack <davidfekete51@gmail.com>
2023-02-18 20:02:37 +0100

    hehe

--- tampermonkey.js
+++ tampermonkey.js
@@ -6,7 +6,7 @@
 // @exclude      view-source://*
 // @exclude      https://classic.grepolis.com/game/*
 // @version      v6.9
-// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require      https://raw.githubusercontent.com/davblack/legalispublic/master/config.js
 // @require      https://raw.githubusercontent.com/davblack/legalis/main/main.js
 // @require      https://raw.githubusercontent.com/davblack/legalis/main//ui.js
 // @require      https://raw.githubusercontent.com/davblack/legalis/main//wonder.js
