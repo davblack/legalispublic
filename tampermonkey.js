c093df3f7272e9b3e52894d20e958e6a1fb42d8d
Davblack <davidfekete51@gmail.com>
2023-02-18 20:06:18 +0100

    v1 aye

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         Legalis
-// @author       focuki es cuki
+// @author       nem mondom el ehehehe 
 // @include      http://*.grepolis.com/game/*
 // @include      https://*.grepolis.com/game/*
 // @exclude      view-source://*
 // @exclude      https://classic.grepolis.com/game/*
 // @version      v6.9
 // @require      https://raw.githubusercontent.com/davblack/legalispublic/master/config.js
-// @require      https://raw.githubusercontent.com/davblack/legalis/main/main.js
-// @require      https://raw.githubusercontent.com/davblack/legalis/main//ui.js
-// @require      https://raw.githubusercontent.com/davblack/legalis/main//wonder.js
+// @require      https://raw.githubusercontent.com/davblack/legalispublic/master/main.js
+// @require      https://raw.githubusercontent.com/davblack/legalispublic/master/ui.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
